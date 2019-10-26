4bc585385e52ed465dd41aa77c9f6c64f80a3cf5
John Paul Lim Gabule <lucas.gabule@gmail.com>
2019-10-26 18:30:12 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+require('dotenv').config({path: 'variables.env'});
+
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+const { graphiqlExpress, graphqlExpress} = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const {typeDefs} = require('./schema');
+const {resolvers} = require('./resolvers');
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log('DB CONNECTED'))
+    .catch(err => console.error(err));
+
+const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true
+};
+
+app.use(cors(corsOptions));
+
+// set up jwt authentication middleware
+app.use(async (req, res, next) => {
+    const token = req.headers['authorization'];
+    console.log(token);
+    next();
+});
+
+
+// create graphiql application
+app.use("/graphiql", graphiqlExpress({endpointURL: "/graphql"}));
+
+// connect schemas with graphql
+app.use("/graphql", bodyParser.json() ,graphqlExpress({
+    schema,
+    context: {
+        Recipe,
+        User
+    }
+}));
+
+const PORT = process.env.PORT || 4444;
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`)
+});
