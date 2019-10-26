c3301fa7c529ad15ad3f16aa3ad2c22cd6c9e837
John Paul Lim Gabule <lucas.gabule@gmail.com>
2019-10-26 22:59:02 +0800

    Adding User-Based Navigation to React

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({path: 'variables.env'});
 
 const Recipe = require('./models/Recipe');
 const User = require('./models/User');
 
-const { graphiqlExpress, graphqlExpress} = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
+const {graphiqlExpress, graphqlExpress} = require('apollo-server-express');
+const {makeExecutableSchema} = require('graphql-tools');
 
 const {typeDefs} = require('./schema');
 const {resolvers} = require('./resolvers');
@@ -34,8 +35,14 @@ app.use(cors(corsOptions));
 
 // set up jwt authentication middleware
 app.use(async (req, res, next) => {
-    const token = req.headers['authorization'];
-    console.log(token);
+    const token = req.headers["authorization"];
+    if (token !== "null") {
+        try {
+            req.currentUser = await jwt.verify(token, process.env.SECRET);
+        } catch (e) {
+            console.error(e)
+        }
+    }
     next();
 });
 
@@ -44,13 +51,15 @@ app.use(async (req, res, next) => {
 app.use("/graphiql", graphiqlExpress({endpointURL: "/graphql"}));
 
 // connect schemas with graphql
-app.use("/graphql", bodyParser.json() ,graphqlExpress({
-    schema,
-    context: {
-        Recipe,
-        User
-    }
-}));
+app.use("/graphql", bodyParser.json(), graphqlExpress(({currentUser}) => ({
+        schema,
+        context: {
+            Recipe,
+            User,
+            currentUser
+        }
+    }))
+);
 
 const PORT = process.env.PORT || 4444;
 app.listen(PORT, () => {
